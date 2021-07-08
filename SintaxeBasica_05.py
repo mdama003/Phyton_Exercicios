a04ffb1ec2492740b6011792fcd01e5f8037608a
monicat2021 <86492931+monicat2021@users.noreply.github.com>
2021-07-08 18:03:31 -0300

    Add files via upload

---
+++ SintaxeBasica_05.py
@@ -0,0 +1,12 @@
+# Float - Número de ponto flutuante com casas decimais
+
+num3 = 12.8
+# outra forma de declarar um número como float
+num4 = float(19.0)
+# Ao declarar um número decimal como inteiro o compilador entende que o número não tem casas decimais
+num5 = int(16.5)
+
+print (num3)
+print (num5)
+# mostra a classe que o número pertence, neste caso float
+print (type(num4))
---
+++ SintaxeBasica_06.py
@@ -0,0 +1,11 @@
+# String - Texto composto de qualquer caracter alfanumérico
+
+palavra1 = 'palavra'
+# Strings podem ser colocadas entre aspas simples ou duplas
+# neste caso foi usado aspas duplas porque a palavra tem aspas simples
+palavra2 =  "Marca d'água"
+frase1 = 'Uma frase qualquer'
+frase2 = 'Ela tem 8 anos'
+frase3 = str('outra frase qualquer')
+print (palavra2)
+print (type(frase3))
